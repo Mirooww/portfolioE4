b37e54fc5b0223d8427bbd9911a415be85c316d3
[Mirooww] <[jordanlansoypro@gmail.com]>
2024-04-06 22:31:39 +0200

    ajout de l'api et du fetch API ainsi qu'un minimum de disposition quant à ce fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,46 @@ import "./style/leftMenuNav.css";
 import "./style/app.css";
 import "./style/content.css";
 
+import { formation } from "./api/formation";
+import { stage } from "./api/stage";
+
+import getFormation from "./hooks/getFormation";
+
 function App() {
-    return (
-        <>
-            <div className="leftMenuNav">
-                <header>
-                    <div className="PPFrame">
-                        <img src="/pictures/PP.jpeg" alt="" />
-                    </div>
-                    <h2 className="name">Jordan LANSOY</h2>
-                    <div className="social">
-                        <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                    </div>
-                </header>
-                <nav>
-                    <div>Accueil</div>
-                    <div>A Propos</div>
-                    <div>Parcours</div>
-                    <div>Epreuve E4</div>
-                    <div>Contact</div>
-                </nav>
-                <footer>
-                    <p> COPYRIGHT 2024 @Nexum</p>
-                </footer>
+   return (
+      <>
+         <div className="leftMenuNav">
+            <header>
+               <div className="PPFrame">
+                  <img src="/pictures/PP.jpeg" alt="" />
+               </div>
+               <h2 className="name">Jordan LANSOY</h2>
+               <div className="social">
+                  <div>1</div>
+                  <div>2</div>
+                  <div>3</div>
+               </div>
+            </header>
+            <nav>
+               <div>Accueil</div>
+               <div>A Propos</div>
+               <div>Parcours</div>
+               <div>Epreuve E4</div>
+               <div>Contact</div>
+            </nav>
+            <footer>
+               <p> COPYRIGHT 2024 @Nexum</p>
+            </footer>
+         </div>
+         <div className="content">
+            <div className="accueil">
+               <h1>BIENVENUE SUR MON PORTFOLIO</h1>
             </div>
-            <div className="content">
-                <div className="accueil">
-                    <h1>BIENVENUE SUR MON PORTFOLIO</h1>
-                </div>
-                <div className="test"></div>
-            </div>
-        </>
-    );
+            <div className="test">{getFormation(formation)}</div>
+            <div className="test">{getFormation(stage)}</div>
+         </div>
+      </>
+   );
 }
 
 export default App;
