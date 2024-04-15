99089f73e5a81796e8f0aa88596682525e9ff386
Jordan LANSOY <jordanlansoypro@gmail.com>
2024-04-15 23:43:08 +0200

    grosses restructuration du portfolio

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -2,57 +2,122 @@ import { BrowserRouter as Route, Routes } from "react-router-dom";
 import "./style/leftMenuNav.css";
 import "./style/app.css";
 import "./style/content.css";
-
-import { formation } from "./api/formation";
-import { stage } from "./api/stage";
-
-import getFormation from "./hooks/getFormation";
+import "./style/stars.sass";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from "@fortawesome/free-regular-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import { formation } from "./json/Projets";
+
+import fetchJson from "./hooks/fetchJson";
+import getFormation from "./hooks/getFormation";
 function MainPage() {
-   return (
-      <>
-         <div className="leftMenuNav">
-            <header>
-               <div className="PPFrame">
-                  <img src="/pictures/PP.jpeg" alt="" />
-               </div>
-               <h2 className="name">CHIENG</h2>
-               <div className="social">
-                  <div>
-                     <FontAwesomeIcon icon={faGithub} style={{ height: "50%" }} />
-                  </div>
-                  <div>
-                     <FontAwesomeIcon icon={faLinkedin} style={{ height: "50%" }} />
-                  </div>
-                  <div style={{ position: "relative" }}>
-                     <FontAwesomeIcon icon={faFile} style={{ height: "70%" }} />
-                     <p style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "8px" }}> CV </p>
-                  </div>
-               </div>
-            </header>
-            <nav>
-               <div>Accueil</div>
-               <div>A Propos</div>
-               <div>Parcours</div>
-               <div>Epreuve E4</div>
-               <div>Contact</div>
-            </nav>
-            <footer>
-               <p> COPYRIGHT 2024 @Nexum</p>
-            </footer>
-         </div>
-         <div className="content">
-            <div className="accueil">
-               <h1>BIENVENUE SUR MON PORTFOLIO</h1>
+    return (
+        <div className="corps">
+            <div className="leftMenuNav">
+                <header>
+                    <div className="PPFrame">
+                        <img src="/pictures/PP.jpeg" alt="photo_profil" />
+                    </div>
+                    <h2 className="name">Jordan LANSOY</h2>
+                    <div className="social">
+                        <div>
+                            <FontAwesomeIcon icon={faGithub} style={{ height: "50%" }} />
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faLinkedin} style={{ height: "50%" }} />
+                        </div>
+                        <div style={{ position: "relative" }}>
+                            <FontAwesomeIcon icon={faFile} style={{ height: "70%" }} />
+                            <p style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "8px" }}> CV </p>
+                        </div>
+                    </div>
+                </header>
+                <nav>
+                    <div>Accueil</div>
+                    <div>A Propos</div>
+                    <div>Parcours</div>
+                    <div>Epreuve E4</div>
+                    <div>Contact</div>
+                </nav>
+                <footer>
+                    <p> COPYRIGHT 2024 @Nexum</p>
+                </footer>
             </div>
-            <div className="test">{getFormation(formation)}</div>
-            <div className="test">{getFormation(stage)}</div>
-         </div>
-      </>
-   );
+
+            <div className="content">
+                <div id="stars"></div>
+                <div id="stars2"></div>
+                <div id="stars3"></div>
+                <div className="accueil">
+                    <h1 className="title">BIENVENUE SUR MON PORTFOLIO</h1>
+                </div>
+                <div className="APropos">
+                    <div className="AProposContainer">
+                        <div className="Cadre">
+                            <h1 className="title">A Propos</h1>
+                            <div className="PresentationBox">
+                                <div className="picturesFrame">
+                                    <img src="/pictures/PP.jpeg" alt="photo_profil" />
+                                </div>
+                                <div className="PresentationContent">
+                                    <h2>Présentation</h2>
+                                    <p> Salut, moi c'est Jordan LANSOY</p>
+                                    <p>
+                                        Je suis actuellement étudiant en 2éme année de BTS SIO (Services Informatiques aux Organisations) option SLAM (Solutions
+                                        Logicielles et Application Métier) à l'EPSI de Lille.
+                                    </p>
+                                    <p>Sur mon portfolio vous retrouverez toutes les notions et projets vu lors de ces 2 années de BTS SIO.</p>
+
+                                    <p>Vous pourrez retrouver mon CV ici :</p>
+                                    <div className="cvContainer">
+                                        <div className="cv">
+                                            <h2>CV</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="BtsSIO">
+                        <h1> Qu'est-ce-que le BTS SIO</h1>
+                        <div>
+                            {" "}
+                            <p>
+                                Le BTS Services Informatiques aux Organisations (SIO) est un diplôme qui prépare en deux ans aux carrières dans l’informatique,
+                                soit en tant qu’administrateur réseau, soit en tant que développeur.
+                            </p>
+                            <p>
+                                Ce cursus est idéal pour ceux qui visent une insertion professionnelle rapide ou qui envisagent de poursuivre leurs études dans
+                                le secteur informatique.
+                            </p>
+                            <p>Le programme du BTS SIO se divise en deux options :</p>
+                        </div>
+
+                        <div className="containerBTS">
+                            <p>
+                                Option SISR (Solutions d’Infrastructure, Systèmes et Réseaux) : Cette spécialité vise à former des experts en réseaux et
+                                systèmes informatiques, couvrant l’installation, la maintenance et la sécurité. Les diplômés de cette option sont qualifiés pour
+                                administrer et sécuriser les réseaux d’entreprise. Les carrières accessibles incluent : Administrateur de systèmes et réseaux
+                                Technicien support et déploiement Responsable d’exploitation Technicien support systèmes et réseaux Technicien d’infrastructure
+                                Technicien de production
+                            </p>
+                            <p>
+                                Technicien micro et réseaux Option SLAM (Solutions Logicielles et Applications Métiers) : Cette spécialité, que vous avez
+                                choisie, forme des professionnels du logiciel, de la conception à l’intégration en entreprise, en passant par la rédaction de
+                                spécifications et le développement. Les métiers envisageables pour les diplômés SLAM sont : Développeur d’applications
+                                informatiques Développeur Analyste d’applications ou d’études Analyste programmeur Programmeur analyste Programmeur
+                                d’applications Responsable des services applicatifs Technicien d’études informatiques Le BTS SIO offre ainsi une formation
+                                solide pour ceux qui aspirent à évoluer dans le domaine dynamique et en constante évolution de l’informatique.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="test">{getFormation(formation)}</div>
+            </div>
+        </div>
+    );
 }
 
 export default MainPage;
--- src/hooks/getFormation.jsx
+++ src/hooks/getFormation.jsx
@@ -1,55 +1,51 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 export default function getFormation(linkApi) {
-   const [formations, setFormations] = useState([]);
+    const [formations, setFormations] = useState([]);
 
-   useEffect(() => {
-      // Ici, nous simulons une requête API en utilisant les données importées directement
-      // Dans un cas réel, vous utiliseriez axios.get(url) pour récupérer les données de l'API
-      const fetchFormations = async () => {
-         try {
-            // Simuler une réponse d'API avec une promesse
-            const response = await Promise.resolve({ data: linkApi });
-            setFormations(response.data);
-         } catch (error) {
-            console.error("Erreur lors de la récupération des formations:", error);
-         }
-      };
+    useEffect(() => {
+        // Ici, nous simulons une requête API en utilisant les données importées directement
+        // Dans un cas réel, vous utiliseriez axios.get(url) pour récupérer les données de l'API
+        const fetchFormations = async () => {
+            try {
+                // Simuler une réponse d'API avec une promesse
+                const response = await Promise.resolve({ data: linkApi });
+                setFormations(response.data);
+            } catch (error) {
+                console.error("Erreur lors de la récupération des formations:", error);
+            }
+        };
 
-      fetchFormations();
-   }, []);
+        fetchFormations();
+    }, []);
 
-   return (
-      <div
-         style={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-around",
-            alignItems: "flex-start",
-            gap: "30px", // Ceci ajoutera de l'espace entre les éléments
-            width: "100%",
-            boxShadow: "0 0 0 1px",
-         }}
-      >
-         {formations.map((formation) => (
-            <div
-               key={formation.id}
-               style={{
-                  width: "300px",
-                  height: "400px",
-                  margin: "50px", // La moitié de 100px pour avoir une marge totale de 100px entre les éléments
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Un peu d'ombre pour chaque élément
-               }}
-            >
-               <h2>{formation.titre}</h2>
-               <p>{formation.description}</p>
-               <div>
-                  {formation.language.map((lang, index) => (
-                     <p key={index}>{lang.name}</p> // Assurez-vous d'ajouter une clé unique pour chaque élément mappé
-                  ))}
-               </div>
-            </div>
-         ))}
-      </div>
-   );
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+                alignItems: "flex-start",
+                gap: "30px", // Ceci ajoutera de l'espace entre les éléments
+                width: "100%",
+                boxShadow: "0 0 0 1px",
+                height: "100vh",
+            }}
+        >
+            {formations.map((formation) => (
+                <div key={formation.id}>
+                    <h2>{formation.title}</h2>
+                    <p>{formation.description}</p>
+                    <p>Date de début: {formation.startdate}</p>
+                    <p>Date de fin: {formation.enddate}</p>
+                    <p>Stage: {formation.stage}</p>
+                    <ul>
+                        {formation.Competences.map((competence, index) => (
+                            <li key={index}>{competence}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
 }
